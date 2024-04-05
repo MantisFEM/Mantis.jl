a313146cf8a00eb0bdbd4aa0bf8ccf38d27e6020
Deepesh Toshniwal <localadmin@tud501648.kpn>
2024-04-05 14:53:13 +0200

    test: add tensor-product tests (basic + with unstructured struct)

--- test/FiniteElementSpaces/runtests.jl
+++ test/FiniteElementSpaces/runtests.jl
@@ -4,5 +4,6 @@ using Test
 
 @testset "BezierExtraction" begin include("BezierExtractionTest.jl") end
 @testset "UnivariateSplineSpaces" begin include("UnivariateSplineTests.jl") end
+@testset "TensorProductTests" begin include("TensorProductTests.jl") end
 
 end
