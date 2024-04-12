16d620b4fbca51ef691482bf7e2664f42898c4dd
Deepesh Toshniwal <localadmin@tud501648.kpn>
2024-04-12 18:19:00 +0200

    add plotter for basis function values

--- test/FiniteElementSpaces/DraftChainedGeometry.jl
+++ test/FiniteElementSpaces/DraftChainedGeometry.jl
@@ -1,4 +1,4 @@
-import Mantis
+import Mantis, LinearAlgebra, Plots
 
 abstract type AbstractFunctionSpace end
 
@@ -173,17 +173,37 @@ end
 function evaluate_der(us_space::NewUnstructuredSpace, element_id::Int, xi::Vector{Float64})
     extraction_coefficients, basis_indices = get_extraction(us_space, element_id)
     us_dbasis, _, x = get_local_dbasis(us_space, element_id, xi)
-    us_basis .= @views us_dbasis * extraction_coefficients
+    us_dbasis .= @views us_dbasis * extraction_coefficients
 
     return us_dbasis, basis_indices, x
 end
 
-####### EXAMPLES OF APPLICATIONS
+####### PLOTTING
+function plot_basis_vals(fem_space::AbstractFunctionSpace, xi::Vector{Float64})
+    Vals = Vector{Array{Float64}}(undef, get_dim(fem_space))
+    for i = 1:get_dim(fem_space)
+        Vals[i] = [[] []]
+    end
+    for el = 1:get_num_elements(fem_space)
+        vals, inds, x = evaluate_val(fem_space, el, xi)
+        for i = 1:length(inds)
+            Vals[inds[i]] = [Vals[inds[i]]; [x vals[:,i]]]
+        end
+    end
+    p = Plots.plot()
+    for i = 1:get_dim(fem_space)
+        Plots.plot!(p, Vals[i][:,1], Vals[i][:,2])
+    end
+    Plots.display(p)
+end
 
-import LinearAlgebra
+####### EXAMPLES OF APPLICATIONS
 
 # polynomial degree
 deg = 3
+# plotting points
+nplt = 21
+xi_plot = collect(LinRange(0.0,1.0,nplt))
 
 #### LEVEL 0
 # starting space with trivial geometry
@@ -191,10 +211,15 @@ B0 = NewElementSpace(deg)
 
 #### LEVEL 1
 # Mapped discontinuous spaces with non-trivial analytical geometry
-map11(xi) = 0.5 * xi.^2; dmap11(xi) = xi
+map11(xi) = 0.25 * xi.^2 + 0.25 * xi; dmap11(xi) = 0.5 * xi .+ 0.25
 map12(xi) = 0.5 * xi .+ 0.5; dmap12(xi) = 0.5 * ones(size(xi))
 B11 = NewFEMSpace(B0, AnalGeometry(map11,dmap11))
 B12 = NewFEMSpace(B0, AnalGeometry(map12,dmap12))
+
+plot_basis_vals(B11, xi_plot)
+plot_basis_vals(B12, xi_plot)
+
+# # (B0, geom1, geom2) -> (B0, geom1), (B0, geom2) -> multi-patch space
 
 # evaluations at end points
 B11_val, inds11, x11 = evaluate_val(B11, 1, [1.0])
@@ -224,6 +249,8 @@ space_dim = n11+n12-2
 # combined space
 B1 = NewUnstructuredSpace((B11, B12),Mantis.FiniteElementSpaces.ExtractionOperator(extraction_coeffs,basis_indices,num_elements,space_dim))
 
+plot_basis_vals(B1, xi_plot)
+
 #### LEVEL 2
 # Mapped discontinuous spaces with non-trivial FEM geometry
 geom21 = FEMGeometry(LinRange(0.0, 2.0, get_dim(B1)), B1)
@@ -231,33 +258,39 @@ geom22 = FEMGeometry(LinRange(0.0, 2.0, get_dim(B1)).^2 .+ 2, B1)
 B21 = NewFEMSpace(B1, geom21)
 B22 = NewFEMSpace(B1, geom22)
 
-# evaluations at end points
-B21_val, inds21, x21 = evaluate_val(B21, 1, [1.0])
-B22_val, inds22, x22 = evaluate_val(B22, 1, [0.0])
-B21_der, _, x21 = evaluate_der(B21, 1, [1.0])
-B22_der, _, x22 = evaluate_der(B22, 1, [0.0])
-n21 = get_dim(B21)
-n22 = get_dim(B22)
-# extraction operator computation
-g_extraction_coefficients = zeros(n21+n22, n21+n22-2)
-g_extraction_coefficients[1:n21-1, 1:n21-1] = Matrix(LinearAlgebra.I, n21-1,n21-1)
-g_extraction_coefficients[end-n22+2:end, end-n22+2:end] = Matrix(LinearAlgebra.I, n22-1,n22-1)
-constraint_vec = [-B21_der[end-1], -B21_der[end]+B22_der[1], B22_der[2]]
-alpha = -constraint_vec[1]./constraint_vec[2]
-beta = -constraint_vec[3]./constraint_vec[2]
-g_extraction_coefficients[n21:n21+1,n21-1] .= alpha
-g_extraction_coefficients[n21:n21+1,n21] .= beta
+plot_basis_vals(B21, xi_plot)
+plot_basis_vals(B22, xi_plot)
 
-extraction_coeffs = Vector{Array{Float64}}(undef,4)
+readline()
+
+# # evaluations at end points
+# B21_val, inds21, x21 = evaluate_val(B21, 1, [1.0])
+# B22_val, inds22, x22 = evaluate_val(B22, 1, [0.0])
+# B21_der, _, x21 = evaluate_der(B21, 1, [1.0])
+# B22_der, _, x22 = evaluate_der(B22, 1, [0.0])
+# n21 = get_dim(B21)
+# n22 = get_dim(B22)
+# # extraction operator computation
+# g_extraction_coefficients = zeros(n21+n22, n21+n22-2)
+# g_extraction_coefficients[1:n21-1, 1:n21-1] = Matrix(LinearAlgebra.I, n21-1,n21-1)
+# g_extraction_coefficients[end-n22+2:end, end-n22+2:end] = Matrix(LinearAlgebra.I, n22-1,n22-1)
+# constraint_vec = [-B21_der[end-1], -B21_der[end]+B22_der[1], B22_der[2]]
+# alpha = -constraint_vec[1]./constraint_vec[2]
+# beta = -constraint_vec[3]./constraint_vec[2]
+# g_extraction_coefficients[n21:n21+1,n21-1] .= alpha
+# g_extraction_coefficients[n21:n21+1,n21] .= beta
+# g_extraction_coefficients_1 = g_extraction_coefficients[1:n21,:]
+# g_extraction_coefficients_2 = g_extraction_coefficients[n21+1:end,:]
+# extraction_coeffs = Vector{Array{Float64}}(undef,4)
 # extraction_coeffs[1] = g_extraction_coefficients[1:deg+1,1:deg+1]
-# extraction_coeffs[2] = g_extraction_coefficients[deg+2:end,deg:end]
+# extraction_coeffs[2] = g_extraction_coefficients[deg+2:end,deg:2*deg]
 
-basis_indices = Vector{Vector{Int}}(undef,4)
-# basis_indices[1] = 1:n21
-# basis_indices[2] = n21-1:n21+n22-2
+# basis_indices = Vector{Vector{Int}}(undef,4)
+# # basis_indices[1] = 1:n21
+# # basis_indices[2] = n21-1:n21+n22-2
 
-num_elements = 4
-space_dim = n21+n22-2
+# num_elements = 4
+# space_dim = n21+n22-2
 
-# combined space
-B2 = NewUnstructuredSpace((B21, B22),Mantis.FiniteElementSpaces.ExtractionOperator(extraction_coeffs,basis_indices,num_elements,space_dim))
+# # # combined space
+# # B2 = NewUnstructuredSpace((B21, B22),Mantis.FiniteElementSpaces.ExtractionOperator(extraction_coeffs,basis_indices,num_elements,space_dim))
