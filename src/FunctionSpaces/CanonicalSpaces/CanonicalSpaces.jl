b2c09ef09767fa074509f63dc676efd38fc28581
Deepesh Toshniwal <localadmin@tud501648.kpn>
2024-04-17 17:33:12 +0200

    fix: remove documentation for callable element-spaces struct

--- src/FunctionSpaces/CanonicalSpaces/CanonicalSpaces.jl
+++ src/FunctionSpaces/CanonicalSpaces/CanonicalSpaces.jl
@@ -11,27 +11,27 @@ include("LagrangePolynomials.jl")
 include("ECTSpaces.jl")
 
 
-# Has to be below the include statements to ensure that all evaluate 
-# methods are visible to it!
-@doc raw"""
-    (elem_loc_basis::AbstractCanonicalSpace)(xi::Vector{Float64}, args...)::Array{Float64}
+# # Has to be below the include statements to ensure that all evaluate 
+# # methods are visible to it!
+# @doc raw"""
+#     (elem_loc_basis::AbstractCanonicalSpace)(xi::Vector{Float64}, args...)::Array{Float64}
 
-Call the `evaluate`-method for the given `elem_loc_basis`.
+# Call the `evaluate`-method for the given `elem_loc_basis`.
 
-Wrapper for all `evaluate`-methods so that all `AbstractCanonicalSpace` can 
-be called by calling the struct instead of explicitly calling the 
-`evaluate`-method. Automatically throws a MethodError if the subtype 
-does not have an evaluate function implemented. Some of these methods 
-may have additional arguments, such as the additional derivatives to 
-evaluate, so this version should allow that as well.
+# Wrapper for all `evaluate`-methods so that all `AbstractCanonicalSpace` can 
+# be called by calling the struct instead of explicitly calling the 
+# `evaluate`-method. Automatically throws a MethodError if the subtype 
+# does not have an evaluate function implemented. Some of these methods 
+# may have additional arguments, such as the additional derivatives to 
+# evaluate, so this version should allow that as well.
 
-See also 
-- [`evaluate(elem_loc_basis::AbstractLagrangePolynomials, ξ::Vector{Float64})`](@ref),
-- [`evaluate(elem_loc_basis::Bernstein, xi::Vector{Float64}, nderivatives::Int64)`](@ref),
-- [`evaluate(elem_loc_basis::Bernstein, xi::Vector{Float64})`](@ref),
-- [`evaluate(elem_loc_basis::Bernstein, xi::Float64)`](@ref),
-- [`evaluate(elem_loc_basis::Bernstein, xi::Float64, nderivatives::Int64)`](@ref).
-"""
-function (elem_loc_basis::T where {T <: AbstractCanonicalSpace})(xi::Vector{Float64}, args...)::Array{Float64}
+# See also 
+# - [`evaluate(elem_loc_basis::AbstractLagrangePolynomials, ξ::Vector{Float64})`](@ref),
+# - [`evaluate(elem_loc_basis::Bernstein, xi::Vector{Float64}, nderivatives::Int64)`](@ref),
+# - [`evaluate(elem_loc_basis::Bernstein, xi::Vector{Float64})`](@ref),
+# - [`evaluate(elem_loc_basis::Bernstein, xi::Float64)`](@ref),
+# - [`evaluate(elem_loc_basis::Bernstein, xi::Float64, nderivatives::Int64)`](@ref).
+# """
+function (elem_loc_basis::T where {T <: AbstractCanonicalSpace})(xi::Vector{Float64}, args...)
     return evaluate(elem_loc_basis, xi, args...)
 end
