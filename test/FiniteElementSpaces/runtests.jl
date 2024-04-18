8b9ac10606932629d57c6d3b4facbb4bf2404c04
dccabanas <d.costacabanas@tudelft.nl>
2024-04-18 11:45:27 +0200

    fix: Added Hierarchical stuff to FunctionSpaces

--- test/FiniteElementSpaces/runtests.jl
+++ test/FiniteElementSpaces/runtests.jl
@@ -6,6 +6,5 @@ using Test
 @testset "KnotInsertion" begin include("KnotInsertionTest.jl") end
 @testset "UnivariateSplineSpaces" begin include("UnivariateSplineTests.jl") end
 @testset "TensorProductTests" begin include("TensorProductTests.jl") end
-# @testset "KnotInsertion" begin include("KnotInsertionTest.jl") end
 
 end
