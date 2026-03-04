77edcda9607f2ca26665dec95d4a6889e5539a4b
Joey Dekker <J.Dekker-2@tudelft.nl>
2026-03-04 09:35:05 +0100

    refactor: update the style and remove unneeded docstrings.

--- src/Forms/FormExpressions/ConstantFormSpaces.jl
+++ src/Forms/FormExpressions/ConstantFormSpaces.jl
@@ -3,10 +3,12 @@
 ############################################################################################
 
 """
-    ConstantFormSpace{manifold_dim, form_rank, G, L} <: AbstractFormSpace{manifold_dim, form_rank}
+    ConstantFormSpace{manifold_dim, form_rank, G, L} <:
+    AbstractFormSpace{manifold_dim, form_rank}
 
 Concrete implementation of a space for constant differential forms. For instance, this can
-be used as a Lagrange multiplier enforcing a zero-average constraint on another differential form.
+be used as a Lagrange multiplier enforcing a zero-average constraint on another
+differential form.
 
 # Fields
 - `geometry::G`: The geometry of the domain
@@ -29,8 +31,8 @@ struct ConstantFormSpace{manifold_dim, form_rank, G, L} <:
 
     """
         ConstantFormSpace(
-            form_rank::Int, geometry::G, label::L
-        ) where {manifold_dim, G <: Geometry.AbstractGeometry{manifold_dim}, L <: AbstractString}
+            form_rank::Int, geometry::G, label::AbstractString
+        ) where {manifold_dim, G <: Geometry.AbstractGeometry{manifold_dim}}
 
     Constructor for constant differential form spaces.
 
@@ -62,54 +64,12 @@ end
 #                                   Getters and setters                                    #
 ############################################################################################
 
-"""
-    get_num_basis(::ConstantFormSpace)
+get_num_basis(::ConstantFormSpace) = 1
 
-Get the number of basis functions for the constant form space.
+get_num_basis(::ConstantFormSpace, ::Int) = 1
 
-# Returns
-- `Int`: Number of basis functions (1).
-"""
-function get_num_basis(::ConstantFormSpace)
-    return 1
-end
+get_max_local_dim(::ConstantFormSpace) = 1
 
-"""
-    get_num_basis(::ConstantFormSpace, ::Int)
-
-Get the number of basis functions for the constant form space on a specific element.
-
-# Arguments
-- `::ConstantFormSpace`: The constant form space.
-- `::Int`: The parametric element identifier.
-
-# Returns
-- `Int`: Number of basis functions (1).
-"""
-function get_num_basis(::ConstantFormSpace, ::Int)
-    return 1
-end
-
-"""
-    get_max_local_dim(::ConstantFormSpace)
-
-Get the maximum local dimension of the constant form space.
-
-# Returns
-- `Int`: Maximum local dimension (1).
-"""
-function get_max_local_dim(::ConstantFormSpace)
-    return 1
-end
-
-"""
-    get_estimated_nnz_per_elem(::ConstantFormSpace)
-
-Get the number of non-zero entries per element.
-
-# Returns
-- `Int`: Number of non-zeros per element (1).
-"""
 get_estimated_nnz_per_elem(::ConstantFormSpace) = 1
 
 """
@@ -131,16 +91,16 @@ get_form(form_space::ConstantFormSpace) = form_space
 
 """
     evaluate(
-        ::ConstantFormSpace{manifold_dim, 0},
-        ::Int,
+        form_space::ConstantFormSpace{manifold_dim, 0},
+        element_id::Int,
         xi::Points.AbstractPoints{manifold_dim},
     ) where {manifold_dim}
 
 Evaluate the basis function of a 0-form constant space at given canonical points `xi`.
 
 # Arguments
-- `::ConstantFormSpace{manifold_dim, 0}`: The constant 0-form space.
-- `::Int`: The parametric element identifier.
+- `form_space::ConstantFormSpace{manifold_dim, 0}`: The constant 0-form space.
+- `element_id::Int`: The parametric element identifier.
 - `xi::Points.AbstractPoints{manifold_dim}`: The set of canonical points.
 
 # Returns
@@ -150,7 +110,9 @@ Evaluate the basis function of a 0-form constant space at given canonical points
     of the element, always equal to `[[1]]` in this case.
 """
 function evaluate(
-    ::ConstantFormSpace{manifold_dim, 0}, ::Int, xi::Points.AbstractPoints{manifold_dim}
+    form_space::ConstantFormSpace{manifold_dim, 0},
+    element_id::Int,
+    xi::Points.AbstractPoints{manifold_dim},
 ) where {manifold_dim}
     num_evaluation_points = Points.get_num_points(xi)
     return [ones(Float64, num_evaluation_points, 1)], [[1]]
@@ -158,17 +120,17 @@ end
 
 """
     evaluate(
-        form_space::ConstantFormSpace{manifold_dim, manifold_dim},
-        element_idx::Int,
+        form_space::ConstantFormSpace{manifold_dim, manifold_dim, G},
+        element_id::Int,
         xi::Points.AbstractPoints{manifold_dim},
-    ) where {manifold_dim}
+    ) where {manifold_dim, G <: Geometry.AbstractGeometry{manifold_dim}}
 
 Evaluate the basis function of a top-level (volume) constant form space at given canonical points
-`xi` mapped to the parametric element given by `element_idx`.
+`xi` mapped to the parametric element given by `element_id`.
 
 # Arguments
 - `form_space::ConstantFormSpace{manifold_dim, manifold_dim}`: The constant volume form space.
-- `element_idx::Int`: The parametric element identifier.
+- `element_id::Int`: The parametric element identifier.
 - `xi::Points.AbstractPoints{manifold_dim}`: The set of canonical points.
 
 # Returns
@@ -179,11 +141,11 @@ Evaluate the basis function of a top-level (volume) constant form space at given
 """
 function evaluate(
     form_space::ConstantFormSpace{manifold_dim, manifold_dim, G},
-    element_idx::Int,
+    element_id::Int,
     xi::Points.AbstractPoints{manifold_dim},
 ) where {manifold_dim, G <: Geometry.AbstractGeometry{manifold_dim}}
     num_evaluation_points = Points.get_num_points(xi)
-    J = Geometry.jacobian(form_space.geometry, element_idx, xi)  # Jₖⱼ = ∂Φᵏ\\∂ξⱼ
+    J = Geometry.jacobian(get_geometry(form_space), element_id, xi)  # Jₖⱼ = ∂Φᵏ\\∂ξⱼ
     form_eval = [ones(Float64, num_evaluation_points, 1)]
     form_eval[1][:] .*= LinearAlgebra.det.(J)
 
