82248dc58c20b634321549b9c3c78461e18c5556
Diogo C. Cabanas <dccabanas-dev@tuta.com>
2026-07-14 11:36:51 +0200

    test: add `Points` module tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,14 +3,15 @@ module MantisTests
 using Pkg
 using Test
 
+@testset verbose=true "GeneralHelpers" begin include("GeneralHelpers/runtests.jl") end
 @testset verbose=true "Mesh" begin include("Mesh/runtests.jl") end
-@testset verbose=true "Quadrature" begin include("Quadrature/runtests.jl") end
-@testset verbose=true "FunctionSpaces" begin include("FunctionSpaces/runtests.jl") end
+@testset verbose=true "Points" begin include("Points/runtests.jl") end
 @testset verbose=true "Geometry" begin include("Geometry/runtests.jl") end
+@testset verbose=true "FunctionSpaces" begin include("FunctionSpaces/runtests.jl") end
+@testset verbose=true "Quadrature" begin include("Quadrature/runtests.jl") end
 @testset verbose=true "Forms" begin include("Forms/runtests.jl") end
 @testset verbose=true "Assembly" begin include("Assemblers/runtests.jl") end
 @testset verbose=true "Plot" begin include("Plot/runtests.jl") end
-@testset verbose=true "GeneralHelpers" begin include("GeneralHelpers/runtests.jl") end
 
 # Do not run JET tests on pre-release versions, as JET is too unstable and the CI will
 # appear as failing. Also only run it from v1.12 onwards, also for compatibility reasons.
