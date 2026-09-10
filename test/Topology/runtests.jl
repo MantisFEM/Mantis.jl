26644a0b50cb51e929aa212d753e50f53c377381
Joey Dekker <J.Dekker-2@tudelft.nl>
2026-09-10 15:48:43 +0200

    test: add topology tests

--- test/Topology/runtests.jl
+++ test/Topology/runtests.jl
@@ -2,6 +2,14 @@ module TopologyTests
 
 using Test
 
-@testset "Topology" begin include("TopologyTests.jl") end
+@testset "Topology" verbose=true begin
+    @testset "Patches" verbose=true begin
+        include("PatchesTests.jl")
+    end
+    @testset "MeshTopology" verbose=true begin
+        include("MeshTopologyTests.jl")
+    end
+    #include("TopologyTests.jl")
+end
 
 end
